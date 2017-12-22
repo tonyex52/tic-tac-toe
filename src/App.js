0fc3af5641035cfcbc3391d7054271b3ab80de9b
tonyex52 <tonyex52@gmail.com>
2017-12-23 04:11:30 +0800

    [mod] modify bind this

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ class Naming extends Component {
   render() {
       return (
         <section className="player-naming">
-          <div>Player 1 <input type="text" onChange={(e) => this.props.assignName({which: 1, name: e.target.value})}/></div>
+          <div>Player 1 <input type="text" className="input-text" placeholder="請輸入玩家1名字" onChange={(e) => this.props.assignName({which: 1, name: e.target.value})}/></div>
           <br/>
-          <div>Player 2 <input type="text"/></div>
+          <div>Player 2 <input type="text" className="input-text" placeholder="請輸入玩家2名字" onChange={(e) => this.props.assignName({which: 2, name: e.target.value})}/></div>
         </section>
       );
   }
@@ -23,20 +23,23 @@ class game extends Component {
   constructor(props) {
       super(props);
       this.state = {
-        player1: '',
-        player2: ''
+        character: {
+          player1: '',
+          player2: ''
+        }
       }
   }
 
   _assignName({which, name}) {
     let nameObject = {}
-    nameObject[`player${which}`] = name
+    nameObject['character'] = JSON.parse(JSON.stringify(this.state.character))
+    nameObject['character'][`player${which}`] = name
     this.setState(nameObject)
   }
 
   render() {
     return (
-      <Naming assignName={this._assignName}/>
+      <Naming assignName={this._assignName.bind(this)}/>
     );
   }
 }
